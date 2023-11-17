a732c5bc96ef2e9d325b5f8db386f5251525d701
Lucy Coates <47534892+ShadowOracle19@users.noreply.github.com>
2023-11-17 13:38:37 -0500

    added camera moving functionality

---
+++ Assets/Scripts/Platformer/CameraMove.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Camera.main.gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, -10);
+        }
+    }
+}
