519f349c9395ed9b2c70c05ffcd319e4faa081f9
LucasCoates <47534892+ShadowOracle19@users.noreply.github.com>
2023-11-25 11:54:22 -0500

    added end level

---
+++ Assets/Scripts/Platformer/EndLevel.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour
+{
+    //when player touches the end point they will spawn at the spawn point
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Check if what the collision is touching is the player
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.transform.position = GameManager.Instance.spawnPoint.position;
+            GameManager.Instance.playerMood += 1;
+        }
+    }
+}
